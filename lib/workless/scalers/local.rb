f0a288e045434080f95f6ee848913d4a9c3fb501
Florent Morin <florent.morin@morin-innovation.com>
2013-07-10 03:28:30 +0200

    Fix

--- lib/workless/scalers/local.rb
+++ lib/workless/scalers/local.rb
@@ -8,7 +8,7 @@ module Delayed
 
         def self.up
           if self.workers == 0
-            Rush::Box.new[Rails.root].bash("script/delayed_job start -i workless", :background => true)
+            Rush::Box.new[Rails.root].bash("bin/delayed_job start -i workless", :background => true)
             sleep 1
           end
           true
@@ -16,7 +16,7 @@ module Delayed
 
         def self.down
           unless jobs.count > 0 and workers > 0
-            Rush::Box.new[Rails.root].bash("script/delayed_job stop -i workless", :background => true)
+            Rush::Box.new[Rails.root].bash("bin/delayed_job stop -i workless", :background => true)
           end
           true
         end
